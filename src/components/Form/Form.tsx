e6f5002f2d39bab7cfe1da00dabbc8431ccfbf34
okibolat <okuevabubakar@gmail.com>
2022-12-20 00:33:01 +0600

    Настроил redux-toolkit

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styles from './Form.module.css';
+import styles from './Form.module.scss';
 
 interface Props {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
---
+++ src/store/reducers/Default.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface Default {
+
+}
+
+const initialState: Default = {
+  
+}
+
+export const Default = createSlice({
+  name: 'default',
+  initialState,
+  reducers: {
+  },
+})
+
+export const { } = Default.actions
+
+export default Default.reducer
